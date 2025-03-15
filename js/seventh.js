514a2991c1b523edb3f690e6ac4219fd69f5eb12
Hlib612 <glebmalonos@gmail.com>
2025-03-15 10:34:53 +0200

    add

---
+++ js/seventh.js
@@ -0,0 +1,31 @@
+// const arr = [1, 2, 3];
+// console.log(arr);
+// arr[1] = 10;
+// console.log(arr);
+
+// const arr1 = ["1", "2", "3"];
+// console.log(arr1);
+// arr1[3] = "4";
+// console.log(arr1);
+
+// const arr2 = [1, 2, 4, 6, 9, 3]
+// let total = 0;
+// for(let i = 0; i < arr2.length; i += 1){
+//     // console.log(arr2[i]);
+//     total = total + arr2[i];
+//     // console.log(total);
+// }
+// console.log(total);
+
+// const arr3 = [14, 25, 41, 67, 90, 34]
+// for(let i = 0; i < arr3.length; i += 1){
+//     console.log(arr3[i]);
+// }
+
+// const arr4 = ["123", "123456", "12", "1234567", "1"]
+// for(let i = 0; i < arr4.length; i += 1){
+//     // console.log(arr4[i]);
+//     if(arr4[i].length > 5){
+//         console.log(arr4[i]);
+//     }
+//     }

e1e6c329f1f8ae2ad65d1ebd5e890bdd1f05a707
Hlib612 <glebmalonos@gmail.com>
2025-03-22 08:51:11 +0200

    add homework 8

---
+++ js/eightth.js
@@ -0,0 +1,45 @@
+// 1)
+// const friend = ["Mango", "Poly", "Kiwi", "Ajax"]
+// let string = "";
+// string = friend.join(',')
+// console.log(string);
+
+
+// 2, 3, 4, 5)
+// const cards = [
+//     'Карточка-1',
+//     'Карточка-2',
+//     'Карточка-3',
+//     'Карточка-4',
+//     'Карточка-5',
+//     ];
+//     // console.log(cards)
+//     // console.log("=====================================")
+//     // const removeCard = cards.splice(2, 1)
+//     // console.log(cards)
+//     // console.log("=====================================")
+//     // const addCard = cards.splice(cards.length + 1, 0, "Карточка-6")
+//     // console.log(cards)
+//     console.log("=====================================")
+//     const updateCard = cards.splice(3, 1, 'Карточка-4-оновлена')
+//     console.log(cards)
+
+
+// const arrayOriginal = ["true", "Kiwi", "Monkong", "Singu", "Mango", "Ajax", "Poly", "false"];
+
+// console.log("arrayOriginal:", arrayOriginal);
+
+// // на масив arrayNew:
+// //todo: ["Mango", "Ajax", "Poly", "Kiwi", "Monkong", "Singu"];
+
+// // не змінюючи значення елементів та використовуючи методи:
+// // push(), pop(), unshift(), shift(),
+// // slice(), splice(), concat() за власним бажанням!
+
+// let arrayNew = [];
+
+// const deleteLast = arrayOriginal.splice(length - 1, 1);
+// const deleteFirst = arrayOriginal.shift(0);
+// arrayNew = arrayOriginal;
+
+// console.log("arrayNew:", arrayNew);

c9952f57dd25ac7a76dd0fd19f633cc6285831af
Hlib612 <glebmalonos@gmail.com>
2025-03-23 13:26:23 +0200

    add some

---
+++ js/nineth.js
@@ -0,0 +1,33 @@
+// let array = [let i = 0; i < array.length; i += 1];
+// let item;
+// let n = 0;
+// const loglogItems = function(array){
+//     for(item of array){
+//         // console.log(item)
+//         console.log(`${n += 1} - ${item}`)
+//     }
+// }
+// loglogItems(["Mango", "Ajax", "Poly"]);
+
+
+// const calculateEngravingPrice = function(message, pricePerWord){
+//     let newMessage = message.split(" ");
+//     let total = newMessage.length * pricePerWord;
+// return total;
+// }
+// console.log(calculateEngravingPrice("Mango, Ajax, Poly", 50));
+
+
+// const findLongestWord = function(string){
+// let array = string.split(" ");
+// let theLongestWord = array[0];
+// for(const word of array){
+//     console.log(word);
+//     console.log(word.length);
+//     if(word.length > theLongestWord.length){
+//         theLongestWord = word ;
+//     }
+// }
+// return `Найдовше слово = ${theLongestWord}`;
+// }
+// console.log(findLongestWord("Mago eat bananas"));

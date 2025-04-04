dd412ac68c59359630c5060de39b4f587bc44099
Hlib612 <glebmalonos@gmail.com>
2025-04-04 21:53:11 +0300

    add

---
+++ js/tenth.js
@@ -0,0 +1,7 @@
+const processArray = (array, callback) => {
+
+}
+
+const getSum = function(arr) {return arr.reduce((acc, curr) => acc + curr)}
+const getMin = function(arr) {return Math.min(...arr)}
+const getMax = function(arr) {return Math.max(...arr)}

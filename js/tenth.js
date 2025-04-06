c1b78c18b8d1949a92ff74c370b0e840e4ddc658
Hlib612 <glebmalonos@gmail.com>
2025-04-06 12:04:53 +0300

    add homework

--- js/tenth.js
+++ js/tenth.js
@@ -1,7 +1,30 @@
+const arr1 = [1, 3, 5, 8, 2, 9];
 const processArray = (array, callback) => {
+    let finish = callback(array);
+    return finish;
+};
+const getSum = (arr) => {
+  return arr.reduce((acc, curr) => acc + curr);
+};
+const getMin = (arr) => {
+  return Math.min(...arr);
+};
+const getMax = (arr) => {
+  return Math.max(...arr);
+};
 
-}
+console.log(processArray(arr1, getSum));
 
-const getSum = function(arr) {return arr.reduce((acc, curr) => acc + curr)}
-const getMin = function(arr) {return Math.min(...arr)}
-const getMax = function(arr) {return Math.max(...arr)}
+console.log("=========================================");
+
+
+const operate = (a, b, callback) => {
+    let finish = callback(a, b);
+     return finish;
+};
+
+// Виклик функції з різними операціями
+console.log(operate(10, 5, (a, b) => { return a - b})); 
+console.log(operate(10, 5, (a, b) => { return a + b})); 
+console.log(operate(10, 5, (a, b) => { return a * b})); 
+console.log(operate(10, 5, (a, b) => { return a / b}));

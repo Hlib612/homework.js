2f93246d213896575b0c864950cab8c3df5d0b86
Hlib612 <glebmalonos@gmail.com>
2025-04-19 10:08:37 +0300

    add

---
+++ js/eleventh.js
@@ -0,0 +1,37 @@
+// const bankAccount = {
+//     ownerName: "Riche-rich",
+//     accountNumber: 65436,
+//     balance: 10000000,
+//     deposit (value){
+//         const sure1 = confirm(`Are you sure you want to deposit on your bank account ${value}`);
+//         if(sure1 == true){
+//             this.balance = this.balance + value;
+//         }else{
+//             return alert("ok :)")
+//         }
+//     },
+//     withdraw (value){
+//         const sure2 = confirm(`Are you sure you want to withdraw from your bank account ${value}`);
+//         if(sure2 == true){
+//             this.balance = this.balance - value;
+//         }else{
+//             return alert("ok :)")
+//         }
+//     },
+// }
+// const action = prompt("What you want: deposit or withdraw?");
+// if(action === "deposit"){
+//     bankAccount.deposit(Number(prompt("How much?")));
+//     alert(`Your balance now ${bankAccount.balance}`)
+// } else if(action === "withdraw"){
+//     const sum = Number(prompt("How much?"));
+//     if(sum < bankAccount.balance){
+//         bankAccount.withdraw(sum);
+//         alert(`Your balance now ${bankAccount.balance}`)
+//     }else{ 
+//         alert(`Sory but your bank account have only ${bankAccount.balance}`)
+//     }
+// } else {
+//     alert("Wrong function")
+// }
+// console.log(bankAccount);

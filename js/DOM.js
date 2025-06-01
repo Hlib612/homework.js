973ed6a8f09391fc379c5f19b4def6685c970d54
Hlib612 <glebmalonos@gmail.com>
2025-06-01 09:20:42 +0300

    add

---
+++ js/DOM.js
@@ -0,0 +1,21 @@
+const buttonJs = document.querySelector('#button_js_id');
+const textForButtonJs = document.querySelector('#text_js_id');
+buttonJs.textContent = textForButtonJs.textContent;
+
+const pictureJs = document.querySelector('.picture_js');
+pictureJs.src = "https://firtka.if.ua/media/cache/blog_thumb/data/blog/266413/16e900eed9b4b6fc7b1e706200e7a013.jpeg";
+
+const avocadoLinkJs = document.querySelector('.link_js');
+avocadoLinkJs.href = "https://en.wikipedia.org/wiki/Apple";
+
+// const fruitsImgJs = documents.querySelector('img_js');
+// fruitsImgJs.src = "https://5.imimg.com/data5/AK/RA/MY-68428614/apple.jpg";
+// fruitsImgJs.alt = "apple";
+// console.log(fruitsImgJs.alt);
+// не спрацювало
+
+const fruitListJs = document.querySelector('list_js');
+console.log(fruitListJs);
+// const fruitListJsFirstChild = fruitListJs.firstElementChild;
+// fruitListJsFirstChild.textContent = "Pineapple";
+// не спрацювало
